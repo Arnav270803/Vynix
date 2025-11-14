44f8ee53a252270f540e2755c98502a1703e5531
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-11-14 10:50:47 +0530

    versal Issue Fixing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
-import AppContextProvider from './context/AppContext.jsx'
+import AppContextProvider from './Context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
